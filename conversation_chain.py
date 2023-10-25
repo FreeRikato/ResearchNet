83925c73464950fc64386d6491e054fcbd58c2b4
FreeRikato <aravinthan@student.tce.edu>
2023-10-26 04:52:27 +0530

    generate embedding for the chunk and build the conversation chain for the chatbot

---
+++ conversation_chain.py
@@ -0,0 +1,17 @@
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
+
+def get_conversation_chain(vectorstore):
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
