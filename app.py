7572a6fbc38962e2e0acc7b665dfdc145514662d
FreeRikato <aravinthan@student.tce.edu>
2023-10-26 04:54:13 +0530

    main python file for the streamlit

--- app.py
+++ app.py
@@ -1,18 +1,59 @@
 import streamlit as st
 from dotenv import load_dotenv
+from htmlTemplates import css, bot_template, user_template
+from pdf_extractor import get_pdf_text
+from text_chunker import get_text_chunks
+from generate_embedding import get_vectorstore
+from conversation_chain import get_conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
-    
-    st.header("Chat with Multiple PDFs :books:")
-    st.text_input("Ask a question about your documents")
-    
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
     with st.sidebar:
-        st.subheader("Document List")
-        st.file_uploader("Upload your PDFs here and click on 'Process' below", accept_multiple_files=True)
-        st.button("Process")
-        
-if __name__ == "__main__":
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
     main()
